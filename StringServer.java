270e9db5ff1a046f5ca62e2f8084159ea105b3fe
jjlenox <joshua.watersjr@gmail.com>
2023-01-30 18:22:58 -0800

    StringServer

--- StringServer.java
+++ StringServer.java
@@ -6,25 +6,26 @@ class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
-    List list = new ArrayList();
+    List<String> list = new ArrayList<String>();
     String StringList = new String("");
-
+    
     public String handleRequest(URI url) {
+        //list.add("Messages: ");
+        //StringList = "";
+        //for (int i = 0; i < list.size(); i++){
+        //        
+        //    }
         if (url.getPath().equals("/")) {
-            for (int i = 0; i < list.size(); i++){
-                StringList = StringList + list[i];
-            }
-            return String.format("Joshua's number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
+            
+            return String.format(StringList);
         } else {
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+            if (url.getPath().contains("/add-message")) {
+            String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    //list.add(parameters[1]);
+                    StringList = StringList + parameters[1] + "\r\n";
+                    return String.format(StringList);
                 }
             }
             return "404 Not Found!";
@@ -32,7 +33,7 @@ class Handler implements URLHandler {
     }
 }
 
-class NumberServer {
+class StringServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
